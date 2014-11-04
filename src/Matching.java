332cea8474cf32d9a7cec96b5ca0b1905352a149
Gustav Henning <ghenning@csc.kth.se>
2014-11-04 18:54:50 +0100

    removed shit

--- src/Matching.java
+++ src/Matching.java
@@ -61,58 +61,6 @@ public class Matching {
 		return -1;
 	}
 
-	public static void calc(int[] X, int[] Y) {
-		Queue queue = new Queue();
-
-		for (int x : X) {
-			int nEx = EdgeList.numE[x];
-
-			if (nEx > 0) {
-				if (nEx == 1) {
-					mark(x, EdgeList.edgesFromX(x)[0], true);
-				} else {
-					visit(x, queue);
-				}
-			}
-		}
-		while (!queue.isEmpty()) {
-			try {
-				int next = (Integer) queue.dequeue();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-
-		}
-
-	}
-
-	private static void visit(int v, Queue queue) {
-		// Alla kanter från v
-		int[] edgesToV;
-		if (v <= maximum.length) {
-			edgesToV = EdgeList.edgesFromX(v);
-		} else {
-			edgesToV = EdgeList.edgesFromY(v);
-		}
-		// Tar den kant med minst antal kanter
-		int smallestEdge = 0;
-		for (int e : edgesToV) {
-			if (!matched[e]) {
-				int numE = EdgeList.numE[e];
-				if (numE < EdgeList.numE[smallestEdge] || smallestEdge == 0) {
-					smallestEdge = e;
-				}
-			}
-		}
-		mark(v, smallestEdge, true);
-		// Köar resten
-		for (int e : edgesToV) {
-			if (e != smallestEdge && !matched[e]) {
-				queue.enqueue(e);
-			}
-		}
-
-	}
 
 	/**
 	 * Marks a pair of x,y as matched. Can be used to do the opposite
