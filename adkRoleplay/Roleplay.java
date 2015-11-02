c5151ebbf29d69546a16579caf70520169ef57aa
Gustav Henning <ghenning@csc.kth.se>
2015-11-02 17:13:46 +0100

    tried something new

--- adkRoleplay/Roleplay.java
+++ adkRoleplay/Roleplay.java
@@ -1,63 +1,68 @@
-
 public class Roleplay {
-		final boolean DEBUG = false;
-		/* 
-		* In: 
-		*	vertices: (v>=1)
-		*	edges: (e>=0)
-		*	goal: (m=>1)
-		*
-		*	for(e)
-		*		e.x e.y
-		* Out:
-		*	roles (n=>1)
-		*	scenes: (s=>1)
-		*	actors: (k=>2)
-		*	for(roles)
-		*		k1, k2 ... etc
-		*	for(scenes)
-		*		n1, n2 ... etc
-		*/
+	final boolean DEBUG = true;
 
-		public Roleplay() {
-			Kattio io = new Kattio(System.in, System.out);
-			int v = io.getInt();
-			int e = io.getInt();
-			int m = io.getInt();
+	/*
+	 * In: vertices: (v>=1) edges: (e>=0) goal: (m=>1)
+	 * 
+	 * for(e) e.x e.y Out: roles (n=>1) scenes: (s=>1) actors: (k=>2) for(roles)
+	 * k1, k2 ... etc for(scenes) n1, n2 ... etc
+	 */
 
-			int n = v+1;
-			int s = e;
-			int k = m+2;
+	public Roleplay() {
+		Kattio io = new Kattio(System.in, System.out);
+		int v = io.getInt();
+		int e = io.getInt();
+		int m = io.getInt();
 
-			if(k < 3)
-				k = 3;
-			if(DEBUG)
-				System.out.println("n s k");
-			System.out.println(n + " " + s + " " + k);
-			/* Let all roles be played by any actor (except divas) */
-			StringBuilder sb = new StringBuilder("" + (k-2));
-			for(int i = 1; i <= k-2; i++){
-				sb.append(" " + (i+2));
-			}
-				/* roles */
-			if(DEBUG)
-				System.out.println("roles");
-			for(int i = 1; i < n; i++){
-				System.out.println(sb.toString());
-			}
-			System.out.println("2 1 2"); //last fictional node have divas 1 & 2
-			if(DEBUG)
-				System.out.println("scenes");
-			for(int i = 0; i < s; i++){
-				int n1 = io.getInt();
-				int n2 = io.getInt();
-				System.out.println("3 " + n1 + " " + n2 + " " + n);
-			}
+		int n = v + 1;
+		int s = e;
+		int k = m + 1;
 
+		if (k < 3)
+			k = 3;
+		if (DEBUG)
+			System.out.println("n s k");
+		System.out.println(n + " " + 1 + " " + k);
+		/* Let all roles be played by any actor (except divas) */
+		StringBuilder sb = new StringBuilder("" + (k - 2));
+		for (int i = 1; i <= k - 2; i++) {
+			sb.append(" " + (i + 2));
+		}
+		/* roles */
+		if (DEBUG)
+			System.out.println("roles");
+		for (int i = 1; i < n; i++) {
+			System.out.println(sb.toString());
+		}
+		System.out.println("2 1 2"); // last fictional node have divas 1 & 2
+		if (DEBUG)
+			System.out.println("scenes");
+
+		int[] val = new int[n];
+		for (int i = 0; i < s; i++) {
+			val[io.getInt()]++;
+			val[io.getInt()]++;
+		}
+		int maxVal = 1;
+		for (int i = 1; i < val.length; i++) {
+			maxVal = (val[i] > maxVal) ? val[i] : maxVal;
+		}
+		sb = new StringBuilder((val[maxVal] + 1) + " ");
+		for (int j = 1; j < (val[maxVal] + 1); j++) {
+			sb.append(j + " ");
+		}
+		sb.append(n);
+		System.out.println(sb.toString());
+		// for (int i = 0; i < s; i++) {
+		// int n1 = io.getInt();
+		// int n2 = io.getInt();
+		// System.out.println("3 " + n1 + " " + n2 + " " + n);
+		// }
+		if (!DEBUG)
 			io.close();
-		}
-
-		public static void main(String[] args){
-			new Roleplay();
-		}
 	}
+
+	public static void main(String[] args) {
+		new Roleplay();
+	}
+}
--- adkRoleplay/Test.java
+++ adkRoleplay/Test.java
@@ -1,20 +1,32 @@
 
-
-
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 
 public class Test {
+	Roleplay r;
 	public Test() {
-		test("2 1 2" +
-				" 1 2");
+		System.out.println("y");
+		test("2 1 2" + " 1 2");
+		System.out.println("y");
+		test("3 3 3" + " 1 2" + " 2 3" + " 3 1");
+		System.out.println("y");
+		test("4 3 2" + " 1 2 2 3 3 4");
+		System.out.println("n");
+		test("4 6 2" + " 1 2 2 3 3 4 1 4 2 4 1 3");
+		System.out.println("n");
+		test("4 6 3" + " 1 2 2 3 3 4 1 4 2 4 1 3");
+		System.out.println("y");
+		test("4 6 4" + " 1 2 2 3 3 4 1 4 2 4 1 3");
 	}
 
 	public void test(String t) {
+		System.out.println("Test: " + t);
+		System.out.println();
 		ByteArrayInputStream stream = new ByteArrayInputStream(
 				t.getBytes(StandardCharsets.UTF_8));
 		System.setIn(stream);
-		new Roleplay();
+		r = new Roleplay(); // set debug to true
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
